07fbb875c640c5453de072cfe6659c8099971cf9
WalterHahn <darth.hahn@gmail.com>
2019-09-15 12:18:52 -0700

    added default colors to SQL install script

--- install.sql
+++ install.sql
@@ -8,3 +8,12 @@ CREATE TABLE IF NOT EXISTS colors (
     name VARCHAR(32),
     hex VARCHAR(6)
 );
+
+INSERT INTO colors (
+    created_at,
+    hex,
+    name
+) VALUES 
+(NOW(), 'FF0000', 'red'),
+(NOW(), '00FF00', 'green'),
+(NOW(), '0000FF', 'blue');
